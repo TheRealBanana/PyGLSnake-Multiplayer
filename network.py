5d48b7a5dfdd05b433203d782c4645e8c357cb55
Kyle <TheRealBanana@users.noreply.github.com>
2018-05-05 17:40:47 -0700

    more progress
    Took a break for a few days to work on other stuff. Worked on it a bit today and got the client and server parts talking to each other correctly. I think the threads all quit like they should although the close function callback is broken and wont ever work. Will have to just implement a periodic cleanup function instead.

--- network.py
+++ network.py
@@ -6,6 +6,10 @@ import cPickle
 import Queue
 from sys import exit as sys_exit
 
+#None of our exit methods work from inside here because of the way
+#the GLUT mainloop works. Lets redefine sys_exit to be a callback
+#into the GLUT loop so we can kill it from there.
+
 # Networking stuff for the multiplayer version of PyGlSnake
 
 
@@ -34,41 +38,62 @@ MAX_PLAYERS = 3
 #Was planning on using queues to communicate between threads but the
 #windows select() function can only handle sockets. Lame brah.
 class ClientMode(threading.Thread):
-    def __init__(self, renderman_instance, quit_socket, server_ip, server_port):
+    def __init__(self, renderman_instance, server_ip, server_port):
         self.renderman_instance = renderman_instance
         self.server_ip = server_ip
-        self.server_port = server_port
-        self.quit_socket = quit_socket
+        self.server_port = int(server_port)
+        self.quit_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.quit_socket.bind(("127.0.0.1", 0))
+        self.quit_port = self.quit_socket.getsockname()[1]
         self.main_socket = None
+        self.quitting = False
         super(ClientMode, self).__init__()
         
     #Main part of our thread
     def run(self):
+        self.quit_socket.listen(1)
         #Try and initialize a connection with our server. If we can't its already over.
         #Using streaming TCP sockets. UDP is overkill for what we are doing.
         self.main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.main_socket.connect((self.server_ip, self.server_port))
+        self.main_socket.setblocking(1)
+        try:
+            self.main_socket.connect((self.server_ip, self.server_port))
+            print "Connected to game server. Waiting for data..."
+        except:
+            print "Problem connecting to the server. Check the IP/port in your config. Quitting..."
+            sys_exit()
         
         #Ok we have a good connection to the server. Lets listen for any data
         while self.quitting == False:
+            print "ONE"
             data = None
             try:
-                readable, _, _ = select.select([self.client_socket, self.quit_socket], [], [], 60) #60 second timeout
+                readable, _, _ = select.select([self.main_socket, self.quit_socket], [], [], 60) #60 second timeout
                 for sock in readable:
                     if sock == self.main_socket:
-                        data = self.main_socket.recv(4096)
+                        print "FOUR"
+                        data = self.main_socket.recv(8192)
+                        print "FUCK: (%s) : %s" % (len(data), data)
                         #Handle any commands we get
-                        if data is not None and len(data) > 0:
-                            self.processess_command(data)
+                        if len(data) == 0:
+                            print "ZERO LEN QUIT"
+                            self.quit_thread()
+                        elif data is not None and len(data) > 0:
+                            print "FIVE"
+                            self.process_command(data)
+                        
                         continue
                     elif sock == self.quit_socket:
+                        print "QUIT SOCKET"
                         self.quit_socket.accept()
                         self.quit_socket.close()
                         self.quitting = True
-                        continue
-            except:
+                        break
+            except Exception as e:
+                print "CM DEBUG: %s" % str(e)
                 break
-        
+            sleep(1)
+        print "NINE"
         #Got the message to quit or the thread died, either way lets quit
         self.quit_thread()
     
@@ -78,12 +103,14 @@ class ClientMode(threading.Thread):
             data = cPickle.loads(rawdata)
             if isinstance(data, list) is False:
                 print "Pickled data was not a list, something is horribly wrong here so im quiting..."
-                sys_exit()
+                #sys_exit()
+                return
         except:
             #Major error, blow up
             print "Command processing error, couldn't load pickel data."
-            sys_exit()
-        
+            #sys_exit()
+            return
+        print "RECEIVED VALID COMMAND: %s" % data[0]
         if data[0] == "GET_NEXT_MOVE":
             #Snake reference here, have to change the entire way snake ticks and works tho. Lame.
             pass
@@ -103,6 +130,7 @@ class ClientMode(threading.Thread):
             pass
     
     def quit_thread(self):
+        print "QUIT CALLED"
         try:
             self.main_socket.send("DISCON")
         except:
@@ -113,9 +141,11 @@ class ClientMode(threading.Thread):
             pass
 
 class ServerMode(threading.Thread):
-    def __init__(self, renderman_instance, quit_socket, server_ip, server_port):
+    def __init__(self, renderman_instance, server_ip, server_port):
         self.renderman_instance = renderman_instance
-        self.quit_socket = quit_socket
+        self.quit_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.quit_socket.bind(("127.0.0.1", 0))
+        self.quit_port = self.quit_socket.getsockname()[1]
         self.connections = {}
         self.nextidx = 0
         self.server_ip = server_ip
@@ -133,14 +163,17 @@ class ServerMode(threading.Thread):
         print "SERVER MODE INIT - Settings:"
         print server_ip
         print server_port
+        print self.quit_port
         
     
     def stop_client_threads(self):
         while len(self.connections) > 0:
             #Really dont want to be modifying a list while looping over it
             thread_idx_to_pop = []
-            for index, cli_thread in self.connections.iteritems():
-                if cli_thread.isAlive is False:
+            current_connections = self.connections.keys()
+            for index in current_connections:
+                cli_thread = self.connections[index]
+                if cli_thread.is_alive() is False:
                     cli_thread.join()
                     thread_idx_to_pop.append(index)
                 else:
@@ -150,53 +183,65 @@ class ServerMode(threading.Thread):
                     del(self.connections[i])
         
             
-    def close_connection_callback(self, connection_obj):
+    def close_connection_callback(self, connection_id):
         #Try to join the thread
-        connection_obj.join()
-        del(self.connections[connection_obj.connection_id])
+        #THIS ISNT WORKING
+        self.connections[connection_id].join()
+        del(self.connections[connection_id])
+    
+    
+    def send_command(self, command, client_idx):
+        command = cPickle.dumps(command)
+        if self.connections[client_idx].is_alive() is True:
+            self.connections[client_idx].send_command(command)
+        else:
+            del(self.connections[client_idx])
+    
+    def broadcast_command(self, command):
+        command = cPickle.dumps(command)
+        current_clients = self.connections.keys()
+        for client_idx in current_clients:
+            if self.connections[client_idx].is_alive() is True:
+                self.connections[client_idx].send_command(command)
+            else:
+                del(self.connections[client_idx])
     
     
     #And this is where everything gets really damn complicated Im sure
     def start_game(self):
-        #__cool_multiplayer_game_stuff__()
-        pass
+        #Lets just send everyone a hello and tell them to get lost
+        self.broadcast_command("HELLO")
+        self.broadcast_command("DISCON")
+        print "Said hello, quitting..."
+        self.quitting = True
+        return True
+        
     
     
-    def get_connection(self):
-        #Connect loop
-        while self.quitting is False:
-            while len(self.connections) < MAX_PLAYERS:    
-                print "Waiting for players... Currently %s/%s" % (len(self.connections), MAX_PLAYERS)
-                try:
-                    print "-----------------"
-                    print "HEEEEEEEY!"
-                    readable, _, _ = select.select([self.main_socket], [], [], 60)
-                    for sock in readable:
-                        if sock == self.main_socket:
-                            connection, addy = sock.accept()
-                            connection.setblocking(1)
-                            #Create new thread to handle connection
-                            connection_thread = ServerClientConnection(connection, self.close_connection_callback, self.nextidx)
-                            connection_thread.start()
-                            #Add our connection_thread to our connection tracker
-                            self.connections[self.nextidx] = connection_thread
-                            self.nextidx += 1
-                        elif sock == self.shutdown_socket:
-                            self.shutdown_socket.accept()
-                            self.shutdown_socket.close()
-                            self.quitting = True
-                        return
-                except Exception as e:
-                    print "EXCEPTION!"
-                    print e
-                    self.quitting = True
-                    return
-#                    continue
-                return
-            print "All players are connected! Starting game in %s seconds..." % 10 # REPLACEME LATER WITH VAR
-            print "not really tho, sorry   :("
+    def get_connection(self):  
+        print "Waiting for players... Currently %s/%s" % (len(self.connections), MAX_PLAYERS)
+        print "-----------------"
+        readable, _, _ = select.select([self.main_socket, self.quit_socket], [], [], 60)
+        for sock in readable:
+            if sock == self.main_socket:
+                connection, addy = sock.accept()
+                connection.setblocking(1)
+                #Create new thread to handle connection
+                connection_thread = ServerClientConnection(connection, self.close_connection_callback, self.nextidx)
+                connection_thread.start()
+                #Add our connection_thread to our connection tracker
+                self.connections[self.nextidx] = connection_thread
+                self.nextidx += 1
+            elif sock == self.quit_socket:
+                print "SHUTDOWN SOCKET"
+                self.quit_socket.accept()
+                self.quit_socket.close()
+                self.quitting = True
+                return True
+            continue
     
     def run(self):
+        self.quit_socket.listen(1)
         #Set up our listening socket
         self.main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
@@ -213,12 +258,12 @@ class ServerMode(threading.Thread):
         print "Listening for player at ip: %s  on port %s..." % (self.server_ip, self.server_port)
         
         #This should halt run() until we get the quit signal 
-        while self.quitting is False:
+        while self.quitting is False and len(self.connections) < MAX_PLAYERS:
             self.get_connection()
-            #Would you like to play a game?
-            #self.start_game()
-            
-         
+        if self.quitting is False:
+            print "All players are connected! Starting game in %s seconds..." % 10 # REPLACEME LATER WITH VAR
+            self.start_game()
+        
         #make sure everything has shut down
         self.stop_client_threads()
         try:
@@ -226,7 +271,7 @@ class ServerMode(threading.Thread):
         except:
             pass
         try:
-            self.shutdown_socket.close()
+            self.quit_socket.close()
         except:
             pass
         
@@ -240,12 +285,12 @@ class ServerClientConnection(threading.Thread):
         self.close_connection_callback = close_connection_callback
         self.connection_id = connection_id
         self.quitting = False
-        super(ClientMode, self).__init__()
+        super(ServerClientConnection, self).__init__()
         
     def quit_thread(self):
         self.quitting = True
         self.client_socket.close()
-        self.close_callback()
+        self.close_connection_callback(self.connection_id)
         
     def send_command(self, command):
         try:
@@ -254,28 +299,29 @@ class ServerClientConnection(threading.Thread):
             if command == "":
                 self.receive_data()
         except Exception as e:
-            print "Something went terribly wrong here, Exception was:"
+            print "SCC_DEBUG: Something went terribly wrong here, Exception was:"
             print e
     
     def receive_data(self):
         data = None
-        readable, _, _ = select.select([self.main_socket], [], [], 60)
-        for sock in readable:
-            data = self.client_socket.recv(4096)
-            
-            return data
+        try:
+            readable, _, _ = select.select([self.client_socket], [], [], 60)
+            for sock in readable:
+                data = self.sock.recv(4096)
+                return data
+        except:
+            print "SCC: Client connection error or something else went wrong."
+            self.quit_thread()
+        
+    def run(self):
+        while self.quitting is False:
+            self.receive_data()
 
 
 #Just a simple class to manage the client or server modes. Makes managing the connection from the main thread less dependent on the exact mode
 class MasterNetworkMode(object):
     #Mode_data should be a dictionary where each key is a variable name for the associated mode.
     def __init__(self, renderman_instance, mode_data):
-        #Again, using sockets to communicate between threads here because the
-        #windows select() function can only handle sockets. Lame brah.
-        self.quit_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.quit_socket.bind(("127.0.0.1", 0))
-        self.shutdown_port = self.quit_socket.getsockname()[1] #Need the port number to call back later
-        self.quit_socket.listen(1)
         self.renderman_instance = renderman_instance
         self.thread_instance = None
         self.netmode = mode_data["net_mode"]
@@ -285,21 +331,24 @@ class MasterNetworkMode(object):
     def startNetwork(self):
         if self.thread_instance is None:
             if self.netmode == "ClientMode":
-                self.thread_instance = ClientMode(self.renderman_instance, self.quit_socket, self.mode_data["ip"], self.mode_data["port"])
+                self.thread_instance = ClientMode(self.renderman_instance, self.mode_data["ip"], self.mode_data["port"])
             elif self.netmode == "ServerMode":
-                self.thread_instance = ServerMode(self.renderman_instance, self.quit_socket, self.mode_data["ip"], self.mode_data["port"])
+                self.thread_instance = ServerMode(self.renderman_instance, self.mode_data["ip"], self.mode_data["port"])
             self.thread_instance.start()
         else:
             print "Networking thread is already running."
     
     def stopNetwork(self):
         if self.thread_instance is not None:
-            shutdown_socket_sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            try:
-                shutdown_socket_sender.connect(("127.0.0.1", self.shutdown_port))
-                shutdown_socket_sender.close()
-            except:
-                pass
+            if self.thread_instance.is_alive() is True:
+                shutdown_socket_sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                try:
+                    shutdown_socket_sender.connect(("127.0.0.1", self.thread_instance.quit_port))
+                    shutdown_socket_sender.close()
+                except:
+                    pass
+            else:
+                self.thread_instance = None
         else:
             print "Networking thread is not currently running."
     

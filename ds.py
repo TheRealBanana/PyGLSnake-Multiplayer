26912fe3c781e3d70ca91ee9743143bf21e12e1a
Kyle <TheRealBanana@users.noreply.github.com>
2018-05-14 22:27:30 -0700

    Simple Dedicated server
    Just a simple "dedicated server", if you want to call it that, so that I can run just the network portion without needing all the OpenGL stuff with it. You can see the effect of passing different game mode params here than what the clients each define in PyGLSnake-Multiplayer.py

---
+++ ds.py
@@ -0,0 +1,14 @@
+#Simple server, needed something without all the GL crap to make testing easier
+
+from network import ServerMode
+
+params = {}
+params["WINDOW_SIZE"] = (500,500)
+params["GRID_SIDE_SIZE_PX"] = 25
+params["TICKRATE_MS"] = 200
+params["MATH_PRECISION"] = 5
+
+if __name__ == "__main__":
+    server = ServerMode("0.0.0.0", 33450, params)
+    server.start()
+    
